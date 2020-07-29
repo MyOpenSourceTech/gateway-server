5bc4395be7a1a47ace5be497cb34967fd248b89e
Sourav Kabiraj <sourav.kabiraj@outlook.com>
2020-07-29 18:49:42 +0530

    #GS-003 | Sourav | Added configuration for travis ci. [Updated test]

--- src/test/java/com/coffeebrew/gatewayserver/services/AuthenticationServiceTest.java
+++ src/test/java/com/coffeebrew/gatewayserver/services/AuthenticationServiceTest.java
@@ -1,19 +1,32 @@
 package com.coffeebrew.gatewayserver.services;
 
+import com.coffeebrew.gatewayserver.configs.AuthenticationConfig;
+import com.coffeebrew.gatewayserver.models.Endpoint;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class AuthenticationServiceTest {
 
-    @Autowired
+    @Mock
+    AuthenticationConfig authenticationConfig;
+
+    @InjectMocks
     AuthenticationService target;
 
     @Test
     void shouldReturnTrueForWhitelistedEndpoints() {
+        ArrayList<Endpoint> endpoints = new ArrayList<>();
+        endpoints.add(new Endpoint("/blog/id;GET"));
+        when(authenticationConfig.getAllWhiteListedEndpoints()).thenReturn(endpoints);
+
         boolean whitelisted = target.isWhitelisted("/blog/id", "GET");
 
         assertTrue(whitelisted);
